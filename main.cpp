81430aab513d809ee833d115dc1032c8af8d3bb8
thanizmy <127229049+thanizmy@users.noreply.github.com>
2023-12-03 23:38:27 -0500

    Update main.cpp
    Update menu

--- main.cpp
+++ main.cpp
@@ -5,27 +5,40 @@ int main() {
 	Graph movie;
 	movie.ReadFile();
 	movie.CreateGraph();
-	char userSelectionNum;
+	char userMainSelect;
+	int userSelectionNum;
 	string userInput;
-	cout << "Welcome to CineGenius!" << endl << "Find your next favourite movie within our catalog of over 20k movies." << endl << "------------------------------------------------------------------------------------" << endl;
-	cout << "Recommender Menu" << endl << "Select a filter to search our dataset and we'll share our top 5 similar movies!" << endl;
-	cout << "Type 1 to search by IMDB movie ID, 2 to search by genre, and 3 to search by  movie name: ";
-	cin >> userSelectionNum;
+	while (true) {
+		cout << "Welcome to CineGenius!" << endl << "Find your next favourite movie within our catalog of over 20k movies." << endl << "------------------------------------------------------------------------------------" << endl;
+		cout << "Type 1 to start/continue, 2 to exit: " << endl;
+		cin >> userMainSelect;
+		if (userMainSelect == '2')
+		{
+			cout << "Goodbye! " << endl;
+			break;
+		}
+		cout << "Recommender Menu" << endl << "Select a filter to search our dataset and we'll share our top 5 similar movies!" << endl;
+		cout << "Type 1 to search by IMDB movie ID, 2 to search by genre, and 3 to search by  movie name: ";
+		cin >> userSelectionNum;
 
-	if (userSelectionNum == '1')
-	{
-		cout << "Enter your IMDB movie ID: ";
-	}
-	else if (userSelectionNum == '2')
-	{
-		cout << "Enter your genre: ";
-	}
-	else if (userSelectionNum == '3')
-	{
-		cout << "Enter your movie name: ";
-	}
-	cin >> userInput;
+		if (userSelectionNum == 1)
+		{
+			cout << "Enter your IMDB movie ID: ";
+		}
+		else if (userSelectionNum == 2)
+		{
+			cout << "Enter your genre: ";
+		}
+		else if (userSelectionNum == 3)
+		{
+			cout << "Enter your movie name: ";
+		}
+		else {  // Invalid
+			cout << "Invalid choice! Please try again!";
+			continue;
+		}
+		cin >> userInput;
 
-	movie.bfsSearch(userInput, userSelectionNum);
-	movie.dfsSearch(userInput, userSelectionNum);
+		movie.Search(userInput, userSelectionNum);
+	}
 }
