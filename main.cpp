4817ea9900ee97641def2b6bbaacea9c1d12a3f1
thanizmy <127229049+thanizmy@users.noreply.github.com>
2023-12-04 21:58:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,13 +14,16 @@ int main() {
 		cout << "Welcome to CineGenius!" << endl << "Find your next favourite movie within our catalog of over thousands of movies." << endl << "------------------------------------------------------------------------------------" << endl;
 		cout << "Type 1 to start/continue, other key to exit: ";
 		cin >> userMainSelect;
+		cout << endl;
 		if (userMainSelect == '2')
 		{
 			cout << "Goodbye! " << endl;
 			break;
 		}
 		cout << "Recommender Menu" << endl << "Select a filter to search our dataset and we'll share our top 10 similar movies!" << endl;
-		cout << "Type 1 to search by IMDB movie ID, 2 to search by genre, and 3 to search by movie name: ";
+		cout << "Type: " << endl;;
+		cout << "1 to search by IMDB movie ID \n2 to search by genre \n3 to search by movie name\n";
+		cout << "Enter your choice: ";
 		cin >> userSelectionNum;
 
 		if (userSelectionNum == 1)
